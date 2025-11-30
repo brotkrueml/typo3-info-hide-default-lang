ad15be7c9cdd4cd481d99ec95b9d1349d5c7395b
Chris Müller <coding@brotkrueml.dev>
2025-11-30 10:50:08 +0100

    Update code style configuration

--- ecs.php
+++ ecs.php
@@ -2,26 +2,30 @@
 
 declare (strict_types=1);
 
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
-return static function (ECSConfig $config): void {
-    $header = <<<HEADER
+$header = <<<HEADER
 This file is part of the "info_hide_default_lang" extension for TYPO3 CMS.
 
 For the full copyright and license information, please read the
 LICENSE.txt file that was distributed with this source code.
 HEADER;
 
-    $config->import(__DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php');
-
-    $config->parallel();
-    $config->paths([
+return ECSConfig::configure()
+    ->withSets([
+        __DIR__ . '/.Build/vendor/brotkrueml/coding-standards/config/common.php',
+    ])
+    ->withParallel()
+    ->withPaths([
         __DIR__ . '/Classes',
         __DIR__ . '/Configuration',
-    ]);
-    $config->ruleWithConfiguration(\PhpCsFixer\Fixer\Comment\HeaderCommentFixer::class, [
-        'comment_type' => 'comment',
-        'header' => $header,
-        'separate' => 'both',
-    ]);
-};
+    ])
+    ->withConfiguredRule(
+        HeaderCommentFixer::class,
+        [
+            'comment_type' => 'comment',
+            'header' => $header,
+            'separate' => 'both',
+        ]
+    );
